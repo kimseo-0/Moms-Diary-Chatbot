85a9160aac9a77debe2ad337a34a973d46cc2f0d
sy <42965120+kimseo-0@users.noreply.github.com>
2025-10-30 01:31:59 +0900

    clear

--- src/agent/models.py
+++
@@ -1,8 +0,0 @@
-from langchain_openai import ChatOpenAI, OpenAIEmbeddings
-from dotenv import load_dotenv
-load_dotenv()
-
-# 라우팅 및 제어용 (빠르고 저렴한 모델)
-llm_ctrl = ChatOpenAI(model="gpt-4.1-mini", temperature=0) 
-# 답변 생성 및 RAG용 (고성능 모델)
-llm_agent = ChatOpenAI(model="gpt-4o", temperature=0)
